9e977e534a0cfee5efc3f1cf643bde8ab1cf0dce
james park <2271862p@student.gla.ac.uk>
2018-03-22 23:35:36 +0000

    sub menu aarrggghh

--- YAPS/views.py
+++ YAPS/views.py
@@ -6,12 +6,14 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
-
+from datetime import datetime
 
 def index(request):
-    
+    response = render(request, 'YAPS/index.html')
 
-    return render(request, 'YAPS/index.html')
+    visitor_cookie_handler(request)
+
+    return response
 
     
 
@@ -81,9 +83,28 @@ def register(request):
             profile_form = UserProfileForm()
         return render(request,'YAPS/register.html',{'user_form': user_form, 'profile_form': profile_form,'registered': registered})
 
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
         
-        
-            
-                
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        request.session['last_visit'] = last_visit_cookie        
+    request.session['visits'] = visits
+
+
                 
     
